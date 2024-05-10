ed2d812c1a948d7b231e9d0583ec6dafde291266
Kaktusryak <denis1243qwerty@gmail.com>
2024-05-10 16:14:56 +0300

    minor

--- apps/questionnaire/src/app/store/questions/questions.selectors.ts
+++ apps/questionnaire/src/app/store/questions/questions.selectors.ts
@@ -1,7 +1,6 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { AppStoreInterface } from '../app.state';
-import { QuestionInterface } from 'libs/questionForm/src/lib/questionForm/models/question.model';
-import { filter, map, of, pipe } from 'rxjs';
+
 
 export const selectFeature =
   createFeatureSelector<AppStoreInterface>('questions');
